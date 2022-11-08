b18c95fa26f4045e70b1909d36a6954ebe3e96f9
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-08 14:57:06 +0200

    Added a helper to highlight active navlinks

---
+++ src/RecordStore.MVC/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RecordStore.MVC.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string IsActive(this IHtmlHelper html, string controller = "", string action = "")
+        {
+            var cssClass = "active";
+            var currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            var currentController = (string)html.ViewContext.RouteData.Values["controller"];
+            if (string.IsNullOrEmpty(controller)) controller = currentController;
+            if (string.IsNullOrEmpty(action)) action = currentAction;
+            return controller == currentController && action == currentAction ? cssClass : string.Empty;
+        }
+    }
+}
