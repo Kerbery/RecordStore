d93b56628687a86a97c52fdff13c9c86add29f8b
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-10 15:22:03 +0200

    Added a basic implementation for a generic repository

---
+++ src/RecordStore.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordStore.Core.Interfaces;
+using RecordStore.Infrastructure.Data;
+using System.Linq.Expressions;
+
+namespace RecordStore.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _table;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _table = _context.Set<T>();
+        }
+
+        public async Task<T> GetAsync(Guid id)
+        {
+            return await _table.FindAsync(id.ToString());
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _table.Where(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _table.Where(filter).ToListAsync();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _table.AddAsync(entity);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _table.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            var entity = await _table.FindAsync(id);
+            _table.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
