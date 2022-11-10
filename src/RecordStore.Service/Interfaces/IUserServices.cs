0c7c3b9f9437b03342202be28b74e68b9b616c63
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-10 15:36:34 +0200

    Added UserServices with read operations

---
+++ src/RecordStore.Service/Interfaces/IUserServices.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RecordStore.Service.Interfaces
+{
+    public interface IUserServices
+    {
+        Task<IdentityUser> GetUser(Guid id);
+        Task<IEnumerable<IdentityUser>> GetUsers();
+    }
+}
---
+++ src/RecordStore.Service/Services/UserServices.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RecordStore.Core.Interfaces;
+using RecordStore.Service.Interfaces;
+
+namespace RecordStore.Service.Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly IRepository<IdentityUser> _repository;
+
+        public UserServices(IRepository<IdentityUser> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IdentityUser> GetUser(Guid id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        public async Task<IEnumerable<IdentityUser>> GetUsers()
+        {
+            return await _repository.GetAllAsync();
+        }
+    }
+}
