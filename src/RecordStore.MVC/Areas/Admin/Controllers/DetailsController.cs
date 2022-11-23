b3e5451bfa0555030f26f31c09cd25393f8c6149
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-23 15:11:56 +0200

    Added an admin page for managing Record related details

---
+++ src/RecordStore.MVC/Areas/Admin/Controllers/DetailsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecordStore.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DetailsController : Controller
+    {
+        // GET: DetailsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
