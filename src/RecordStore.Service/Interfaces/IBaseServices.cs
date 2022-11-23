c3ca6788f169e8dbf5eba004ccfc52a60b259dab
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-23 13:29:15 +0200

    Created a generic BaseServices for common CRUD methods

---
+++ src/RecordStore.Service/Interfaces/IBaseServices.cs
@@ -0,0 +1,13 @@
+﻿using RecordStore.Core.Entities;
+
+namespace RecordStore.Service.Interfaces
+{
+    public interface IBaseServices<T> where T : class, IEntity
+    {
+        Task<T> CreateAsync(T entity);
+        Task DeleteAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(Guid id);
+        Task UpdateAsync(T entity);
+    }
+}
---
+++ src/RecordStore.Service/Services/BaseServices.cs
@@ -0,0 +1,43 @@
+﻿using RecordStore.Core.Entities;
+using RecordStore.Core.Interfaces;
+using RecordStore.Service.Interfaces;
+
+namespace RecordStore.Service.Services
+{
+    public class BaseServices<T> : IBaseServices<T> where T : class, IEntity
+    {
+
+        protected readonly IRepository<T> _repository;
+
+        public BaseServices(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<T> GetAsync(Guid id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _repository.CreateAsync(entity);
+            return entity;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            await _repository.RemoveAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await _repository.UpdateAsync(entity);
+        }
+    }
+}
