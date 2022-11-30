ffe5c3fd5f98b28e08d6279e7dc38221d0db12ba
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-11-30 11:53:13 +0200

    Updated minimum lenght of Formats names

--- src/RecordStore.Core/DTOs/Format/CreateFormatDTO.cs
+++ src/RecordStore.Core/DTOs/Format/CreateFormatDTO.cs
@@ -5,7 +5,7 @@ namespace RecordStore.Core.DTOs.Format
     public class CreateFormatDTO
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
         [Display(Name = "Format")]
         public string Name { get; set; }
     }
